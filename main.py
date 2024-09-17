ca37a635a5918220f92b6fcc8d54a7f4b714c3e4
Savelii <savakh81@gmail.com>
2024-09-18 00:33:04 +0700

    switched to chrome

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 
-driver = webdriver.Firefox()
+driver = webdriver.Chrome()
 
 driver.get("https://www.saucedemo.com/")
 
